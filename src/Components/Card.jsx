b1ab9811b84650c6fc201a7c6d1a81d69d1f49a1
rishav-0 <talukdarrishav88@gmail.com>
2025-04-29 11:36:34 +0530

    changed card

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Card = ({flag,name,conutry,onEdit }) => {
   return (
-    <div className="border p-4 min-w-48 rounded-xl flex items-center gap-4">
-      {flag && <img className="w-8 max-w-8" src={flag} alt="" />}
+    <div className="border p-4 min-w-48 flex flex-col justify-between rounded-xl ">
+      {flag && <img className=" mb-4" src={flag} alt="" />}
       <div className="flex gap-4 items-center  w-full justify-between">
         {conutry && <p className="text-sm font-semibold">{conutry}</p>}
         <p className="text-sm font-semibold">{name}</p>
